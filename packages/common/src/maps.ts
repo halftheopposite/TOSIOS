c6029e67491cbfcde7cb7c86d3d08a274d79f3fd
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2019-09-19 16:31:31 +0200

    Refactor the common maps module. Add better typings for Room options and Select component values  list.

--- packages/common/src/maps.ts
+++ packages/common/src/maps.ts
@@ -1,10 +1,14 @@
-export type Names = 'small' | 'long' | 'big';
+import { IListItem } from './types';
 
-export const List: Array<{
-  value: Names;
-  title: string;
-}> = [
-    { value: 'small', title: 'Small' },
-    { value: 'long', title: 'Long' },
-    { value: 'big', title: 'Big' },
-  ];
+export const List: IListItem[] = [
+  { value: 'small', title: 'Small' },
+  { value: 'long', title: 'Long' },
+  { value: 'big', title: 'Big' },
+];
+
+export const Players: IListItem[] = [
+  { value: 2, title: '2 players' },
+  { value: 4, title: '4 players' },
+  { value: 8, title: '8 players' },
+  { value: 16, title: '16 players' },
+];
