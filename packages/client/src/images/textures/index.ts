a49eabb9f709a373ad1c676d32a687185b84a298
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2019-09-30 22:21:37 +0200

    Add new textures for inputs

--- packages/client/src/images/textures/index.ts
+++ packages/client/src/images/textures/index.ts
@@ -1,5 +1,6 @@
 import * as Grounds from './ground';
 import * as GUI from './gui';
+import * as Inputes from './inputs';
 import * as Particles from './particle';
 import * as Players from './player';
 import * as Props from './prop';
@@ -9,6 +10,7 @@ import * as Weapons from './weapon';
 export {
   Grounds as GroundTextures,
   GUI as GUITextures,
+  Inputes as InputesTextures,
   Particles as ParticleTextures,
   Players as PlayerTextures,
   Props as PropTextures,
