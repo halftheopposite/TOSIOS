66dd1276b96a79a34cec7c312fd95ac2fdead401
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2019-10-19 15:02:50 +0200

    Add new icons

--- packages/client/src/images/icons/index.ts
+++ packages/client/src/images/icons/index.ts
@@ -1,12 +1,16 @@
 
 import accountMultipleIcon from './account-multiple.svg';
+import githubIcon from './github.svg';
 import lockOpenIcon from './lock-open.svg';
 import lockIcon from './lock.svg';
 import refreshIcon from './refresh.svg';
+import twitterIcon from './twitter.svg';
 
 export {
   accountMultipleIcon,
+  githubIcon,
   lockOpenIcon,
   lockIcon,
   refreshIcon,
+  twitterIcon,
 };
