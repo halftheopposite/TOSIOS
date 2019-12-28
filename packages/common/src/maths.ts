30e1a1ba43d86ae83ff395073df5c177b9b30171
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2019-12-28 18:22:22 +0100

    Add a method to shuffle arrays

--- packages/common/src/maths.ts
+++ packages/common/src/maths.ts
@@ -97,3 +97,19 @@ export function snapPosition(pos: number, tileSize: number): number {
     ? -rest
     : tileSize - rest;
 }
+
+/**
+ * Shuffles an array
+ */
+export function shuffleArray(array: any[]) {
+  const result = [...array];
+
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = result[i];
+    result[i] = result[j];
+    result[j] = temp;
+  }
+
+  return result;
+}
