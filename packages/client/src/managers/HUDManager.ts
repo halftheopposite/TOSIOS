596088802f9e3fc3c92af70df65c6198d9ab9683
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2019-12-29 21:56:08 +0100

    Rename method in HUDManager

--- packages/client/src/managers/HUDManager.ts
+++ packages/client/src/managers/HUDManager.ts
@@ -182,7 +182,7 @@ export default class HUDManager extends Container {
     this.renderLogs();
   }
 
-  updatePlayer = (playerId: string, name: string, kills: number, color: string) => {
+  createOrUpdatePlayer = (playerId: string, name: string, kills: number, color: string) => {
     const player = this._playersList[playerId];
     if (player && player.kills === kills) {
       return;
