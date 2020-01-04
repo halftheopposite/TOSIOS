d805150b41815333b78a1a65582e504bfcddaae3
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-01-04 21:29:36 +0100

    Add Bat textures

--- packages/client/src/images/textures/index.ts
+++ packages/client/src/images/textures/index.ts
@@ -1,5 +1,6 @@
 import * as GUI from './gui';
 import * as Inputs from './inputs';
+import * as Monsters from './monsters';
 import * as Particles from './particle';
 import * as Players from './player';
 import * as Props from './prop';
@@ -8,6 +9,7 @@ import * as Weapons from './weapon';
 export {
   GUI as GUITextures,
   Inputs as InputsTextures,
+  Monsters as MonstersTextures,
   Particles as ParticleTextures,
   Players as PlayerTextures,
   Props as PropTextures,
---
+++ packages/client/src/images/textures/monsters/index.ts
@@ -0,0 +1,17 @@
+import { Texture } from 'pixi.js';
+
+import bat1 from './bat-1.png';
+import bat2 from './bat-2.png';
+import bat3 from './bat-3.png';
+import bat4 from './bat-4.png';
+
+// Bat
+const batImages: string[] = [bat1, bat2, bat3, bat4];
+const bat: Texture[] = [];
+for (let i = 0; i < batImages.length; i++) {
+  bat.push(Texture.from(batImages[i]));
+}
+
+export {
+  bat as Bat,
+};
