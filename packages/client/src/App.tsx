0f73dbb4eeab851c79e02b156fc7568a9ca75aae
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-04-26 18:32:30 +0200

    Make App component functional.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,19 @@
 import { Router } from '@reach/router';
-import React, { Component } from 'react';
+import React from 'react';
 
 import Game from './scenes/Game';
 import Home from './scenes/Home';
 
-class App extends Component {
-
-  render() {
-    return (
-      <Router>
-        <Home
-          default={true}
-          path="/"
-        />
-        <Game
-          path="/:roomId"
-        />
-      </Router>
-    );
-  }
+export default function App(): React.ReactElement {
+  return (
+    <Router>
+      <Home
+        default={true}
+        path="/"
+      />
+      <Game
+        path="/:roomId"
+      />
+    </Router>
+  );
 }
-
-export default App;
