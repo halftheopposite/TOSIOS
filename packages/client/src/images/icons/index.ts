dea4d575bb6982104f13ac93732d53adecc922a4
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-05-02 23:15:16 +0200

    Add new magic wand icon

--- packages/client/src/images/icons/index.ts
+++ packages/client/src/images/icons/index.ts
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowLeft } from './arrow-left.svg';
 import { ReactComponent as GitHubIcon } from './github.svg';
 import { ReactComponent as LockOpenIcon } from './lock-open.svg';
 import { ReactComponent as LockClosedIcon } from './lock.svg';
+import { ReactComponent as MagicWand } from './magic-wand.svg';
 import { ReactComponent as RefreshIcon } from './refresh.svg';
 
 export {
@@ -12,5 +13,6 @@ export {
   GitHubIcon,
   LockOpenIcon,
   LockClosedIcon,
+  MagicWand,
   RefreshIcon,
 };
