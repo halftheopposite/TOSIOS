2e3118d05f323f5e6215a94f88def4a749f97133
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-05-03 22:29:54 +0200

    Add new menu icon

--- packages/client/src/images/icons/index.ts
+++ packages/client/src/images/icons/index.ts
@@ -5,6 +5,7 @@ import { ReactComponent as GitHubIcon } from './github.svg';
 import { ReactComponent as LockOpenIcon } from './lock-open.svg';
 import { ReactComponent as LockClosedIcon } from './lock.svg';
 import { ReactComponent as MagicWand } from './magic-wand.svg';
+import { ReactComponent as Menu } from './menu.svg';
 import { ReactComponent as RefreshIcon } from './refresh.svg';
 
 export {
@@ -14,5 +15,6 @@ export {
   LockOpenIcon,
   LockClosedIcon,
   MagicWand,
+  Menu,
   RefreshIcon,
 };
