3ed0a9b3e5c44cf106e9e74c22926c5f4fe556ac
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-07-16 23:09:14 +0200

    Add new blink effect

--- packages/client/src/game/sprites/effects.ts
+++ packages/client/src/game/sprites/effects.ts
@@ -1,6 +1,8 @@
 import { Sprite } from 'pixi.js';
 
 const FLASH_DURATION = 100;
+const BLINK_DURATION = 20;
+const BLINK_COUNT = 5;
 
 export const flash = (sprite: Sprite, tintColor: number, baseColor: number) => {
     sprite.tint = tintColor;
@@ -8,4 +10,18 @@ export const flash = (sprite: Sprite, tintColor: number, baseColor: number) => {
     setTimeout(() => {
         sprite.tint = baseColor;
     }, FLASH_DURATION);
+};
+
+export const blink = (sprite: Sprite) => {
+    let count = 0;
+
+    const interval = setInterval(() => {
+        sprite.visible = !sprite.visible;
+        count++;
+
+        if (count >= BLINK_COUNT) {
+            sprite.visible = true;
+            clearInterval(interval);
+        }
+    }, BLINK_DURATION);
 };
