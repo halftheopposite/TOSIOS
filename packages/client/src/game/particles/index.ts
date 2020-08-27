e1bf53cb6734931b82d95c815b60cff221d60c07
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-08-27 22:54:03 +0200

    Add bubble particles

--- packages/client/src/game/particles/index.ts
+++ packages/client/src/game/particles/index.ts
@@ -1,10 +1,13 @@
+import BubbleConfig from './bubble.json';
 import ImpactConfig from './impact.json';
 import SmokeConfig from './smoke.json';
 import { Texture } from 'pixi.js';
+import bubbleImage from './bubble.png';
 import impactImage from './impact.png';
 import smokeImage from './smoke.png';
 
+const BubbleTexture = Texture.from(bubbleImage);
 const ImpactTexture = Texture.from(impactImage);
 const SmokeTexture = Texture.from(smokeImage);
 
-export { ImpactConfig, ImpactTexture, SmokeConfig, SmokeTexture };
+export { BubbleConfig, BubbleTexture, ImpactConfig, ImpactTexture, SmokeConfig, SmokeTexture };
