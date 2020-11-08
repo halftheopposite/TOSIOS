98e348c9bafa8119c49995b310396baff80192f2
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-11-08 18:50:57 +0100

    Improve analytics tracking by using react-ga.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,8 +2,27 @@ import Home from './scenes/Home';
 import Match from './scenes/Match';
 import React from 'react';
 import { Router } from '@reach/router';
+import { useAnalytics } from './hooks';
+import { useLocation } from '@reach/router';
 
 export default function App(): React.ReactElement {
+    const { pathname } = useLocation();
+    const analytics = useAnalytics();
+
+    /**
+     * Initialize analytics.
+     */
+    React.useEffect(() => {
+        analytics.init();
+    }, [analytics]);
+
+    /**
+     * Listen to page changes.
+     */
+    React.useEffect(() => {
+        analytics.page(pathname);
+    }, [analytics, pathname]);
+
     return (
         <Router>
             <Home default path="/" />
--- packages/client/src/hooks/useAnalytics.tsx
+++ packages/client/src/hooks/useAnalytics.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
+const GA_TRACKING_ID: string | undefined = process.env.REACT_APP_GA_TRACKING_ID;
 
 type EventCategory = 'User' | 'Game' | 'Room';
 type EventAction = 'Create' | 'Rename' | 'Share' | 'Join';
@@ -11,28 +14,36 @@ interface EventArgs {
 }
 
 export function useAnalytics(): {
+    init: () => void;
+    page: (path: string) => void;
     track: (args: EventArgs) => void;
 } {
+    /**
+     * Initialize analytics.
+     */
+    const init = () => {
+        if (GA_TRACKING_ID) {
+            ReactGA.initialize(GA_TRACKING_ID);
+        }
+    };
+
+    /**
+     * Track a page.
+     */
+    const page = (path: string) => {
+        ReactGA.pageview(path);
+    };
+
     /**
      * Track an event.
      */
     const track = (args: EventArgs) => {
-        const { category, action, label, value } = args;
-
-        if (!window.ga) {
-            return;
-        }
-
-        window.ga('send', {
-            hitType: 'event',
-            eventCategory: category,
-            eventAction: action,
-            eventLabel: label,
-            eventValue: value,
-        });
+        ReactGA.event(args);
     };
 
     return {
+        init,
+        page,
         track,
     };
 }
