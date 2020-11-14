635b6ee4b96f5137cdd4e6d3a5444af978347632
Aymeric Chauvin <5473864+halftheopposite@users.noreply.github.com>
2020-11-14 21:04:27 +0100

    Fix Announce hud component

--- packages/client/src/scenes/HUD/Announce.tsx
+++ packages/client/src/scenes/HUD/Announce.tsx
@@ -12,28 +12,33 @@ const TICK = ANNOUNCE_ANIM_TICK / ANNOUNCE_LIFETIME;
  */
 export const Announce = React.memo((props: { announce: string; style?: CSSProperties }): React.ReactElement | null => {
     const { announce, style } = props;
-    const [opacity, setOpacity] = React.useState(0);
+    const [opacity, setOpacity] = React.useState<number>(0);
     const [display, setDisplay] = React.useState<'none' | 'flex'>('none');
+    const intervalRef = React.useRef<number>(0);
 
-    // Whenever the announce changes
-    React.useEffect(() => {
-        setOpacity(1);
-        setDisplay('flex');
+    const launchFade = () => {
         const startedAt = Date.now();
 
         // Calculate how much we must take off each tick
-        const intervalId = setInterval(() => {
-            // Animation ended
+        intervalRef.current = window.setInterval(() => {
             const delta = Date.now() - startedAt;
             if (delta > ANNOUNCE_LIFETIME) {
                 setOpacity(0);
                 setDisplay('none');
-                clearInterval(intervalId);
+                clearInterval(intervalRef.current);
                 return;
             }
 
             setOpacity((prev) => prev - TICK);
         }, ANNOUNCE_ANIM_TICK);
+    };
+
+    // Whenever the announce changes
+    React.useEffect(() => {
+        clearInterval(intervalRef.current);
+        setOpacity(1);
+        setDisplay('flex');
+        launchFade();
     }, [announce]);
 
     if (!announce || !announce.length) {
